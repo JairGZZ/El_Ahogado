88747fb17238c1542bcca1f449f0499196b9b2ff
JairGZZ <jairprox1234@gmail.com>
2025-11-06 21:43:39 -0500

    Agregar clases GameResponse y GameStateResponse para gestionar la información del juego y su estado.

---
+++ El_Ahogado.Server/dto/GameResponse.cs
@@ -0,0 +1,13 @@
+﻿using El_Ahogado.Server.enums;
+
+namespace El_Ahogado.Server.dto
+{
+    public class GameResponse
+    {
+        public Guid GameId { get; set; }
+        public string MaskedWord { get; set; }
+        public int AttemtsLeft { get; set; }
+        public GameStates Status { get; set; } = GameStates.IN_PROGRESS;
+        public string SecretWord { get; set; }
+    }
+}
---
+++ El_Ahogado.Server/dto/GameStateResponse.cs
@@ -0,0 +1,12 @@
+﻿using El_Ahogado.Server.enums;
+
+namespace El_Ahogado.Server.dto
+{
+    public class GameStateResponse
+    {
+        public string MaskedWord { get; set; }
+        public ICollection<char> UsedLetters { get; set; } = new List<char>();
+        public int AttemptsLeft { get; set; }
+        public GameStates Status { get; set; } = GameStates.IN_PROGRESS;
+    }
+}
