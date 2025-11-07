f54793dfa5b1d9d308290887c904a4e35e3cdeca
JairGZZ <jairprox1234@gmail.com>
2025-11-06 21:38:50 -0500

    Agregar GameService para gestionar la lógica del juego, incluyendo la creación de nuevos juegos y manejo de intentos.

---
+++ El_Ahogado.Server/services/GameService.cs
@@ -0,0 +1,96 @@
+﻿namespace El_Ahogado.Server.services;
+
+using El_Ahogado.Server.dto;
+using El_Ahogado.Server.models;
+using El_Ahogado.Server.services;
+using El_Ahogado.Server.utils;
+using System.Text;
+
+public class GameService
+{
+    private readonly IServiceProvider _provider;
+    private readonly Dictionary<Guid, Game> _games = new();
+
+
+    public GameService(IServiceProvider provider)
+    {
+        _provider = provider;
+    }
+
+    public async Task<GameResponse> NewGame()
+    {
+
+        // Creamos un scope temporal para obtener ApiService 
+        using var scope = _provider.CreateScope();
+        var apiService = scope.ServiceProvider.GetRequiredService<ApiService>();
+
+        var secretWord = await apiService.GetRandomWordAsync();
+
+        var newGame = new Game
+        {
+            secretWord = secretWord,
+            wordLength = secretWord.Length,
+            maxAttempts = 5,
+            maskedWord = WordUtils.ConvertToMasked(secretWord)
+        };
+
+        _games.Add(newGame.gameId, newGame);
+
+        return new GameResponse{
+            Status = newGame.status,
+            GameId = newGame.gameId,
+            AttemtsLeft = newGame .attemptsLeft,
+            MaskedWord = newGame.maskedWord,
+            SecretWord = newGame.secretWord
+            
+        }
+        ;
+    }
+    private string BuildMaskedWord(string secretWord, ICollection<char> usedLetters)
+    {
+        var sb = new StringBuilder();
+        var normalizedWord = WordUtils.RemoveAccents(secretWord);
+        foreach (var c in normalizedWord)
+        {
+            sb.Append(usedLetters.Contains(c) ? c : '_');
+        }
+        return sb.ToString();
+    }
+
+    public Game? GetGameById(Guid gameId)
+    {
+        
+        return _games.GetValueOrDefault(gameId);
+    }
+
+
+    public GameStateResponse  MakeGuess(Guid gameId, char letter)
+    {
+        var game = GetGameById(gameId);
+
+        game.usedLetters.Add(letter);
+        game.maskedWord = BuildMaskedWord(game.secretWord, game.usedLetters);
+        game.attemptsLeft = game.attemptsLeft - 1;
+
+
+        if(game.attemptsLeft == 0)
+        {
+            game.status = enums.GameStates.GAME_OVER;
+        }
+        if (game.maskedWord
+        .Equals(WordUtils.RemoveAccents(game.secretWord), StringComparison.Ordinal))
+        {
+            game.status = enums.GameStates.WON;
+        }
+        return new GameStateResponse
+        {
+            MaskedWord = game.maskedWord,
+            Status = game.status,
+            AttemptsLeft = game.attemptsLeft,
+            UsedLetters = game.usedLetters
+        };
+    }
+
+    
+
+}
