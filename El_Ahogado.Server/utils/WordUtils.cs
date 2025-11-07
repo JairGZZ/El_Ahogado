4fb989310b4f1af6e8a1465dbff61ab5aa898021
JairGZZ <jairprox1234@gmail.com>
2025-11-06 21:44:37 -0500

    Agregar clase WordUtils con métodos para convertir palabras a formato enmascarado y eliminar acentos.

---
+++ El_Ahogado.Server/utils/WordUtils.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace El_Ahogado.Server.utils
+{
+    public static class WordUtils
+    {
+        public static string ConvertToMasked(string word)
+        {
+            var x = new StringBuilder();
+         
+            var mask = "";
+            foreach(var c in word)
+            {
+
+                x.Append("_");
+            }
+            return x.ToString();
+
+        }
+
+        public static string RemoveAccents(string texto)
+        {
+            // Normaliza el texto para separar las letras de los acentos
+            string normalizado = texto.Normalize(NormalizationForm.FormD);
+
+            // Construye una nueva cadena omitiendo los caracteres de marca (acentos)
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in normalizado)
+            {
+                if (CharUnicodeInfo.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark)
+                {
+                    sb.Append(c);
+                }
+            }
+
+            // Vuelve a la forma normal (sin combinaciones)
+            return sb.ToString().Normalize(NormalizationForm.FormC);
+        }
+    }
+}
