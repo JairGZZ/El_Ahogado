11a80f1070f6b94e352ee730b1bf69a910265d12
JairGZZ <jairprox1234@gmail.com>
2025-11-06 21:38:32 -0500

    Actualizar configuración de servicios: agregar opciones de serialización JSON y cambiar ApiService a Singleton

--- El_Ahogado.Server/Program.cs
+++ El_Ahogado.Server/Program.cs
@@ -1,11 +1,19 @@
 using El_Ahogado.Server.services;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-builder.Services.AddTransient<ApiService>();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        //con esto ya no se mostraran numeros al convertir los enum a json, si no
+        //como el identificador que le puse
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
+builder.Services.AddSingleton<ApiService>();
+builder.Services.AddSingleton<GameService>();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
